ceb3e7496ebd3753a80377de55a7cec4fd9c5ccf
fieldwind <fiedlwind@sina.com>
2020-11-01 00:25:09 +0800

    two

--- lita_config.rb
+++ lita_config.rb
@@ -30,4 +30,11 @@ Lita.configure do |config|
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
   # config.handlers.some_handler.some_config_key = "value"
-end
+  if ENV['RACK_ENV']=='production'
+    config.robot.adapter = :slack
+    config.redis[:url] = ENV.fetch('REDIS_URL')
+  else
+    config.robot.adapter = :shell
+  end
+  config.adapter.slack.token = ENV.fetch('SLACK_TOKEN','')
+end 
